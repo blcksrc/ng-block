98123baf303def4db4a5882e9adefc4fe5fd0388
Block Source <blcksrc@gmail.com>
2018-09-17 00:03:47 +0100

    Add routing to not found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { HomeComponent } from './pages/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
