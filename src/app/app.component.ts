0ea85d85259afd65c3d9461a56b2916b14751e9c
Block Source <blcksrc@gmail.com>
2018-09-16 23:50:55 +0100

    Add main navigation with routing to home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,16 @@ import { Component } from '@angular/core';
   selector: 'blck-root',
   template: `
     <h1>{{ title }}</h1>
+    <nav>
+      <a routerLink="/home" routerLinkActive="active">
+        Home
+      </a>
+    </nav>
     <router-outlet></router-outlet>
   `,
-  styles: []
+  styles: [
+    `.active { font-weight: bolder; }`
+  ]
 })
 export class AppComponent {
   title = 'App';
---
+++ src/app/pages/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'blck-home',
+  template: `
+    <h2>Home</h2>
+  `,
+  styles: []
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
