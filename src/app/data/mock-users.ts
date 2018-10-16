c492b9de46d81b3a0e26b571883bfbf8253e3769
Block Source <blcksrc@gmail.com>
2018-10-17 00:26:59 +0100

    Angular Tutorial - Part 4

---
+++ src/app/data/mock-users.ts
@@ -0,0 +1,14 @@
+import { User } from '../models/user';
+
+export const USERS: User[] = [
+  { id: 11, name: 'Emma' },
+  { id: 12, name: 'Hudson' },
+  { id: 13, name: 'Olivia' },
+  { id: 14, name: 'Anthony' },
+  { id: 15, name: 'Abigail' },
+  { id: 16, name: 'Samuel' },
+  { id: 17, name: 'Harper' },
+  { id: 18, name: 'Lucas' },
+  { id: 19, name: 'Avery' },
+  { id: 20, name: 'James' }
+];
--- src/app/users/pages/users.component.ts
+++ src/app/users/pages/users.component.ts
@@ -1,43 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 
 import { User } from 'src/app/models/user';
+import { USERS } from 'src/app/data/mock-users';
 
 @Component({
   selector: 'blck-users',
   template: `
     <h2>Users</h2>
 
-    <div>
-      <h3>User Data | {{ user.name | uppercase }}</h3>
+    <ul>
+      <li *ngFor="let user of users"
+        (click)="onSelect(user)"
+        [class.selected]="user === selectedUser">
+        {{ user.id }} - {{ user.name }}
+      </li>
+    </ul>
+
+    <div *ngIf="selectedUser">
+      <h3>User Data | {{ selectedUser.name | uppercase }}</h3>
       
       <h4>User Details</h4>
       <p>
-        <b>Id:</b> {{ user.id }}
+        <b>Id:</b> {{ selectedUser.id }}
         <br>
-        <b>Name:</b> {{ user.name }}
+        <b>Name:</b> {{ selectedUser.name }}
       </p>
 
       <h4>Edit User Details</h4>
       <p>
         <label>Name:
-          <input [(ngModel)]="user.name" placeholder="name">
+          <input [(ngModel)]="selectedUser.name" placeholder="name">
         </label>
       </p>
 
     </div>
   `,
-  styles: []
+  styles: [
+    `.selected { font-weight: bolder; text-decoration: underline; }`,
+    `li:hover { font-weight: bolder; }`,
+  ]
 })
 export class UsersComponent implements OnInit {
 
-  user: User = {
-    id: 1,
-    name: 'John',
-  }
+  users = USERS;
+  selectedUser: User;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(user: User): void {
+    this.selectedUser = user;
   }
 
 }
